01245620bbb0a694748d9033c3c3d82a19c7eb93
Aaron <arnhsmith@gmail.com>
2023-07-23 19:47:53 -0400

    checked code and passed all tests

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,33 @@
 
 def happy_new_year():
     # code goes here!
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    updated_list = list()
+    for int in int_list: 
+        updated_list.append(int**2)
+        # print(updated_list)
+    return updated_list
+    
+    
     pass
 
 def fizzbuzz():
     # code goes here!
+
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        
     pass
